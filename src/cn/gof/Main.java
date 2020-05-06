d5c00c690b1707b4d565c4d96c505d147194313e
祝融 <zhilong_u@foxmail.com>
2020-05-06 23:14:53 +0800

    first

---
+++ src/cn/gof/Main.java
@@ -0,0 +1,10 @@
+package cn.gof;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+	// write your code here
+    }
+}
---
+++ src/cn/gof/hashmap/Test.java
@@ -0,0 +1,14 @@
+package cn.gof.hashmap;
+
+import java.util.HashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("1", 1);
+        hashMap.put("2", 2);
+        hashMap.put("3", 3);
+        System.out.println("OK--TT--OK");
+    }
+}
